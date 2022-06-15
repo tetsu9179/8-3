eea9c53fbd8203cec8425b231dc205b1b9d8498e
tetsu9179 <tetsu8137@gmail.com>
2022-06-15 20:22:38 +0900

    課題提出

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [TaskController::class,"home"]);
+Route::post('/register',[TaskController::class,"taskRegister"]);
+Route::post('/complete',[TaskController::class,"taskComplete"]);
